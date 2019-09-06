28052266645e9db90d177c3bbaf4ddf144d46f10
Nien <wolfzxcv@gmail.com>
2019-09-06 14:39:18 +0100

    Updates

---
+++ precache-manifest.2b209b4639911327a4ba67bda0820bc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a7286143eb29fe6fa8faaadae6c7125",
+    "url": "/Connect-Four/index.html"
+  },
+  {
+    "revision": "2293143d1aeaa34491e5",
+    "url": "/Connect-Four/static/css/main.a1738c8f.chunk.css"
+  },
+  {
+    "revision": "252379e31290de2603e0",
+    "url": "/Connect-Four/static/js/2.072cc882.chunk.js"
+  },
+  {
+    "revision": "2293143d1aeaa34491e5",
+    "url": "/Connect-Four/static/js/main.62a8b994.chunk.js"
+  },
+  {
+    "revision": "5ec8fab371b3bd0dc3d4",
+    "url": "/Connect-Four/static/js/runtime~main.117ffc4c.js"
+  }
+]);
